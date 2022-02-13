e5831bf33aa3f7d0d56c36e2c6b995201fb8e3e7
Noam Shira <noamisrael10@gmail.com>
2022-02-13 10:41:20 +0200

    puzzle1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace targil1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            StreamReader reader = new StreamReader("C:\\Users\\1\\Downloads\\input.txt");
+
+            string file = reader.ReadToEnd();
+            // Console.WriteLine(line);
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (file[i] == '(')
+                    count++;
+                else
+                    count--;
+                if (count == -1)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+
+
+                }
+
+            }
+
+
+            reader.Close();
+
+
+
+
+
+
+
+        }
+    }
+}
