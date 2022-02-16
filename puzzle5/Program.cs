b71dfc330c18cf69c3d1a56c6553713f11db43e7
Noam Shira <noamisrael10@gmail.com>
2022-02-16 18:55:57 +0200

    puzzle5

---
+++ puzzle5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace puzzle5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\1\\Desktop\\noamis.txt";
+            string[] readText = File.ReadAllLines(path, Encoding.UTF8);
+            int count = 0;
+            foreach (string s in readText)
+            {
+
+                bool flag1 = false;
+                bool flag2 = true;
+                int countpnim = 0;
+                char lastCharacter = s[s.Length - 1];
+                if (lastCharacter == 'a' || lastCharacter == 'e' || lastCharacter == 'u' ||
+                    lastCharacter == 'i' || lastCharacter == 'o')
+                    countpnim++;
+                for (int i = 0; i < s.Length-1; i++)
+                {
+                    
+                    if (s[i] == 'a' || s[i] == 'e' || s[i] == 'u' || s[i] == 'i' || s[i] == 'o')
+                        countpnim++;
+
+                    if (s[i] == s[i + 1])
+                        flag1 = true;
+                    var str = string.Concat(s[i], s[i + 1]);
+                    if (str == "ab" || str == "cd" || str == "pq" || str == "xy")
+                        flag2 = false;
+
+                }
+                if ((countpnim >= 3) && (flag1 == true) && (flag2 == true))
+                    count++;
+
+            }
+
+            Console.WriteLine(count);
+
+
+
+
+        }
+    }
+}
